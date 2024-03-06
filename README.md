0d19198e689235864e3df4ea724036cb1a79d562
René Mihula <mihula@provys.com>
2024-03-06 17:24:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ConvertSvc
+
---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Provys.Standard.Xmlss.Extensions;
+
+namespace Provys.Service.ConvertSvc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
